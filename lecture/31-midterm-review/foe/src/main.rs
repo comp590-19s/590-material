04cfb217f84b8e06c645e6f494c15dcc7e42f75c
Kris Jordan <kris@cs.unc.edu>
2019-04-15 15:49:54 +0000

    Add pipe example

---
+++ lecture/31-midterm-review/foe/src/main.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main()  -> std::io::Result<()> {
+
+    let mut file = File::create("foe.txt")?;
+    let mut i = 0u64;
+
+    loop {
+        i += 1;
+        file.write_all(format!("stdout: {}\n", i).as_bytes())?;
+        println!("stdout: {}", i);
+        eprintln!("stderr: {}", i);
+    }
+
+}
